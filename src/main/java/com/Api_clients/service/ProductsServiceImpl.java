b5ebc10cfa2ff2833124c5786ff09dd69c28755a
brittanypallasco2003 <nohemiespinel0@gmail.com>
2024-12-24 16:14:33 -0500

    refactor: Create product service

---
+++ src/main/java/com/Api_clients/service/ProductsServiceImpl.java
@@ -0,0 +1,22 @@
+package com.Api_clients.service;
+
+import com.Api_clients.domain.Product;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class ProductsServiceImpl {
+
+    List <Product> productList= new ArrayList<>(Arrays.asList(
+          new Product(1, "Laptop", 799.99, 10),
+          new Product(2, "Smartphone", 499.99, 25),
+          new Product(3, "Tablet", 299.99, 15),
+          new Product(4, "Smartwatch", 199.99, 30)
+    ));
+
+    public List<Product> getProductList(){
+        return productList;
+    }
+
+}
