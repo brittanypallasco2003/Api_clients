953ad55eac3ada0594c93342d3b33fed52d362d9
brittanypallasco2003 <nohemiespinel0@gmail.com>
2024-12-30 17:34:45 -0500

    refactor: add a service bean to read products from a JSON and define it by default

---
+++ src/main/java/com/Api_clients/service/ProductServiceJSONImpl.java
@@ -0,0 +1,29 @@
+package com.Api_clients.service;
+
+import com.Api_clients.domain.Product;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.util.List;
+
+@Primary
+@Service
+public class ProductServiceJSONImpl implements ProductService {
+
+    @Override
+    public List<Product> getProductList() {
+        List<Product> products;
+        try {
+            products = new ObjectMapper()
+                    .readValue(this.getClass().getResourceAsStream("/products.json"),
+                            new TypeReference<List<Product>>() {
+                            });
+            return products;
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
